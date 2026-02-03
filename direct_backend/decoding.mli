586d80bae4e8af9c975b236956c7fd1506a54c48
Kevin Svetlitski <ksvetlitski@janestreet.com>
2026-02-03 17:42:57 -0500

    Temporarily delete the direct backend to avoid clouding context

--- direct_backend/decoding.mli
+++
@@ -1,7 +0,0 @@
-open! Core
-open! Import
-
-type t
-
-val create : pt_file:Filename.t -> sideband_file:Filename.t -> setup_file:Filename.t -> t
-val decode_one : t -> Backend_intf.Event.t option
--- direct_backend/magic_trace_direct_backend.mli
+++
@@ -1,2 +0,0 @@
-open! Import
-include Backend_intf.S
