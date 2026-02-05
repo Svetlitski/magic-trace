dc18cd09c805b6cc52b6376bfacb83011d582660
Kevin Svetlitski <ksvetlitski@janestreet.com>
2026-02-05 16:35:35 -0500

    Follow the conventions of the old backend as closely as possible. Code **works**, but is still riddled with debugging stubs and other detritus, to be cleaned up later

--- src/trace_writer.ml
+++ src/trace_writer.ml
@@ -710,6 +710,7 @@ let ret t (thread_info : _ Thread_info.t) ~time : unit =
 
 let write_trace_segments (type thread) (t : thread inner) =
   Hashtbl.iter t.thread_info ~f:(fun thread_info ->
+    eprintf "NUMBER OF TRACE SEGMENTS: %d\n" (Nonempty_vec.length thread_info.trace_segments);
     Nonempty_vec.iter thread_info.trace_segments ~f:(fun trace_segment ->
       if Trace_segment.in_filtered_region trace_segment
       then
